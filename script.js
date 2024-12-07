48d43596a86de2f18d801bcc119848446fb0c971
samara <pufie.study@gmail.com>
2024-12-07 12:56:55 -0300

    click no menu funcionando

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function showMenu() {
     let menuMobile = document.querySelector('.menu-mobile');
     let menuIcon = document.querySelector('.icon');
-    
+
     if (menuMobile.classList.contains('open')) {
-        menuMobile.classList.remove('open'); // esconde o menu
-        menuIcon.src = "assets/icons8-menu-50.png"; // icone do menu
+        menuMobile.classList.remove('open');
+        menuIcon.src = "assets/icons8-menu-50.png";
     } else {
-        menuMobile.classList.add('open'); // mostra o menu
-        menuIcon.src = "assets/icons8-x-48.png"; // icone de fechar
+        menuMobile.classList.add('open');
+        menuIcon.src = "assets/icons8-x-48.png";
     }
 }
